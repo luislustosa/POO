1816b7f6a75abf13e02c47740a186f86ba2706c1
luis.lustosa <luis.lustosa@sempreceub.com>
2024-09-24 08:34:50 -0300

    aula de objeto dentro de um objeto

---
+++ umaaula.py
@@ -0,0 +1,18 @@
+class Marca:
+    def __init__(self, nome,pais,importado):
+        self.nome = nome
+        self.pais = pais
+        self.importado = importado
+
+    def getData(self):
+        print(f" A marca {self.nome} é do pais {self.pais}  importado ->{self.importado}")
+
+
+class Veiculo:
+    def __init__(self,modelo,ano,marca):
+        self.modelo = modelo
+        self.ano = ano
+        self.marca = marca
+
+marca1 = Marca("Audi","Alemanha",True)
+veiculo1 = Veiculo("R8",2019,marca1)
